54efc6b580f80721f2d08e799cfa6273bd81c160
Bloddy20Moon <118792974+Bloddy20Moon@users.noreply.github.com>
2024-05-13 03:06:36 -0500

    Agregar Cartas dinamicas responsivas para una mejor vision del usuario, falta poco para acabar :'v

--- src/app/portafolio/portafolio.component.ts
+++ src/app/portafolio/portafolio.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ContactoComponent } from '../contacto/contacto.component';
+import { NgxTypedWriterModule } from 'ngx-typed-writer';
+import { CardsComponent } from '../components/cards/cards.component';
 
 @Component({
   selector: 'app-portafolio',
   standalone: true,
-  imports: [ContactoComponent],
+  imports: [ContactoComponent, NgxTypedWriterModule, CardsComponent],
   templateUrl: './portafolio.component.html',
   styleUrl: './portafolio.component.scss'
 })
