dc0df77c987287b5c955694a4b25e2299167c386
Bloddy20Moon <118792974+Bloddy20Moon@users.noreply.github.com>
2024-05-13 20:51:50 -0500

    Añadí arrays a las cartas para una mejor legibilidad en el html y un mejro manejo en el Ts.

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { link } from 'node:fs';
+
+interface Card {
+  imageUrl: string;
+  icon:string[]
+  link: string[];
+}
 
 @Component({
   selector: 'app-cards',
@@ -8,5 +15,47 @@ import { Component } from '@angular/core';
   styleUrl: './cards.component.scss'
 })
 export class CardsComponent {
+  cards: Card[] = [
+    {
+      imageUrl: 'assets/img/card/zapateria1.png',
+      icon:[
+        'assets/img/card/iconos/angular.png',
+        'assets/img/card/iconos/nestjs.png',
+        'assets/img/card/iconos/mysql.png',
+        
+      ],
+      link: [
+        'https://github.com/Bloddy20Moon/SistemaZapateriaFront'
+      ]
+    },
 
+    {
+      imageUrl: 'assets/img/card/TiendaBar.png',
+      icon: [
+        'assets/img/card/iconos/html.png',
+        'assets/img/card/iconos/css.png',
+        'assets/img/card/iconos/JavaScript.png',
+      ],
+      link: [
+        'https://github.com/Bloddy20Moon/PagosCulqi'
+      ]
+
+    },
+
+    {
+      imageUrl: 'assets/img/card/Cortes.png',
+      icon: [
+        'assets/img/card/iconos/html.png',
+        'assets/img/card/iconos/css.png',
+        'assets/img/card/iconos/JavaScript.png',
+      ],
+      link: [
+        'https://github.com/Bloddy20Moon/Wasap_Bot'
+      ]
+    },
+    
+    
+    
+
+  ];
 }
