d54a3943af49fa5f48c6f03a9daf20e4d7958d06
Bloddy20Moon <118792974+Bloddy20Moon@users.noreply.github.com>
2024-05-18 01:09:10 -0500

    Mejora en el scss (Eliminacion de codigo)

--- src/app/components/infinitecarousel/infinitecarousel.component.ts
+++ src/app/components/infinitecarousel/infinitecarousel.component.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, AfterViewInit } from '@angular/core';
 
-
+interface carousel{
+  imgUrl: string;
+}
 
 @Component({
   selector: 'app-infinitecarousel',
@@ -16,6 +18,38 @@ export class InfinitecarouselComponent  implements AfterViewInit {
     const copy: Node = this.elRef.nativeElement.querySelector(".logos-slide").cloneNode(true);
     this.elRef.nativeElement.querySelector(".logos").appendChild(copy);
   }
-
-  
+   
+/*   carousels: carousel[] = [
+    {
+      imgUrl: 'assets/img/carousel/html.svg',
+    },
+    {
+      imgUrl:'assets/img/carousel/css.svg',
+    },
+    {
+      imgUrl: 'assets/img/carousel/javascript.svg',
+    },
+    {
+      imgUrl: 'assets/img/carousel/ts.svg',
+    },
+    {
+      imgUrl: 'assets/img/carousel/sass.svg',
+    },
+    {
+      imgUrl: 'assets/img/carousel/nodejs.svg',
+    },
+    {
+      imgUrl: 'assets/img/carousel/wordpress.svg',
+    },
+    {
+    imgUrl: 'assets/img/carousel/bootstrap.svg',
+    },
+    {
+      imgUrl: 'assets/img/carousel/php.svg',
+    },
+    {
+      imgUrl: 'assets/img/carousel/elementor.svg',
+    },
+  ];
+   */
 }
